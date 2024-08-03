79d14523823424d92f4a87f4f697db66a5aacecc
Akshat Srivastava <akshatsrivastava625@gmail.com>
2024-08-03 09:47:11 +0530

    add readme

--- SpringBoot_Rest/src/main/java/com/akshat/SpringBoot_Rest/RestJobController.java
+++ SpringBoot_Rest/src/main/java/com/akshat/SpringBoot_Rest/RestJobController.java
@@ -14,6 +14,8 @@ import java.util.List;
 //@Controller
 
 //The @CrossOrigin annotation is used to handle the Cross-Origin Resource Sharing (CORS) in Spring Boot.
+//CORS is a security feature that prevents unauthorized access to resources on a server.
+// It is a security feature that allows restricting resources on a web page to be requested from another domain outside the domain from which the resource originated.
 //By default, it is disabled, and it is only enabled once we add the annotation to the main Spring Boot Application file or the Controller file.
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -23,10 +25,12 @@ public class RestJobController {
     @Autowired
     private JobService service;
 
+
     @GetMapping(path = "jobPosts", produces = "application/json")
     //produces = "application/json" is used to specify the type of response that the method will return.
 //    @ResponseBody
     //The @ResponseBody annotation tells a controller that the object returned is automatically serialized into JSON and passed back into the HttpResponse object.
+    //It can be ignored if we use the @RestController annotation.
     public List<JobPost> getAllJobs() {
         return service.getALlJobs();
     }
--- SpringBoot_Rest/src/main/java/com/akshat/SpringBoot_Rest/model/JobPost.java
+++ SpringBoot_Rest/src/main/java/com/akshat/SpringBoot_Rest/model/JobPost.java
@@ -1,26 +1,41 @@
 package com.akshat.SpringBoot_Rest.model;
 
-
 import java.util.List;
-
-
 import org.springframework.stereotype.Component;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a job post in the system.
+ * This class is a data model for job posts and includes various attributes
+ * related to a job post such as post ID, profile, description, required experience,
+ * and technology stack.
+ *
+ * Annotations:
+ * - @Data: Generates getters, setters, toString, equals, and hashCode methods.
+ * - @NoArgsConstructor: Generates a no-argument constructor.
+ * - @AllArgsConstructor: Generates a constructor with all fields as parameters.
+ * - @Component: Marks this class as a Spring component.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Component
 public class JobPost {
 
+    /** The unique identifier for the job post. */
+    private int postId;
 
-	private int postId;
-	private String postProfile; 
-	private String postDesc;
-	private Integer reqExperience;
-	private List<String> postTechStack;
-	
+    /** The profile or title of the job post. */
+    private String postProfile;
 
+    /** A brief description of the job post. */
+    private String postDesc;
+
+    /** The required experience for the job post. */
+    private Integer reqExperience;
+
+    /** The list of technologies required for the job post. */
+    private List<String> postTechStack;
 }
